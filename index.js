382af3c7981ea71a1bd19bc719aad64008eb8581
Abhinav Roy <48856484+Drake0306@users.noreply.github.com>
2022-07-17 00:51:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const { win, BrowserWindow, ipcMain, Notification } = require('electron');
+const { app } = require('electron/main');
+const path = require('path');
+
+const isDev = !app.isPackaged;
+
+function createWindow() {
+    const win= new  BrowserWindow({
+        width: 1200,
+        height: 800,
+        backgroundColor: "white",
+        webPreferences: {
+            nodeIntegration: false,
+            worldIntegration: true,
+            contextIsolation: true,
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    win.loadFile("index.html");
+}
+
+if(isDev) {
+    require('electron-reload')(__dirname, {
+        electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+    })
+}
+
+ipcMain.on('notify', (_, message) => {
+    new Notification({title: 'Notification', body: message}).show();
+})
+
+app.whenReady().then(createWindow)
